7059f82b4bb48492efa75a3b99db942b02808ed9
junpeng.jiang <junpeng.jiang@kumai-inc.com>
2023-03-14 18:45:02 +0800

    引入Vant重写tabbar

--- src/components/tab-bar/index_2.vue
+++ src/components/tab-bar/index_2.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="tab-bar">
     <van-tabbar v-model="currentIndex" active-color="#ff9854">
-      <van-tabbar-item v-for="(item, index) in tabbarData" :key="item.path" :class="{ active: currentIndex === index }" :to="item.path">
+      <van-tabbar-item v-for="(item, index) in tabbarData" :key="item.path" :class="{ active: currentIndex === index }" :to="item.path" icon="home-o">
         <span>{{ item.text }}</span>
-        <template #icon>
+        <!-- <template #icon>
           <img v-if="currentIndex !== index" :src="getAssetURL(item.image)">
           <img v-else :src="getAssetURL(item.imageActive)">
-        </template>
+        </template> -->
       </van-tabbar-item>
     </van-tabbar>
   </div>
@@ -24,6 +24,15 @@
 
 <style lang="scss" scoped>
 .tab-bar {
+  // 局部定义一个变量：只针对 tab-bar 子元素才生效
+  // --van-tabbar-item-icon-size: 30px !important;
+
+  // 找到类，对类中的CSS属性重写
+  // :deep()找到子组件的类，让子组件的类也可以生效
+  :deep(.van-tabbar-item__icon) {
+    font-size: 30px;
+  }
+
   img {
     height: 24px;
   }
